7f9d76978197050142010b40099636e9123c2597
Victor <vreyes@protonmail.ch>
2020-02-04 04:43:17 -0300

    funciona

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # laBanana
+
+Implementación de laBanana con listas enlazadas en los libros de ordenes para 
+probar performance. Con un input de 10 mil ordenes la implementación con 
+arreglos se demora ~7s mientas que esta ~2s. 
+
+Este codigo no pretende seguir ninguna buena practica, solamente testear performance
--- exchange.js
+++ exchange.js
@@ -1,11 +1,133 @@
-class Exchange {
+class Order {
+    constructor(id, user, type, size, value, executedAmount) {
+        this.id = id
+        this.user = user
+        this.type = type
+        this.size = size
+        this.value = value
+        this.price = value/size
+        this.executedAmount = executedAmount
 
+        this.next = null
+        this.prev = null
+    }
+
+    get isExecuted() {
+        return this.checkExecuted()
+    }
+
+    checkExecuted() {
+        return this.executedAmount === this.size
+
+    }
+
+    get remainingAmount() {
+        return this.calcRemainingAmount()
+    }
+
+    calcRemainingAmount() {
+        return this.size - this.executedAmount
+    }
+}
+
+
+
+
+
+class Book {
     constructor() {
-        this.book = {bid: [], ask: []}
-        this.transactions = []
+        this.head = null
+        this.tail = null
+        this.length = 0
     }
 
 
+
+    getNOrder(index) {
+        let i = 0
+        let order = this.head
+        
+        while(order) {
+            if(i === index) return order
+            i++
+            order = order.next
+        }
+
+        return null
+    }
+
+
+    push({id, user, type, size, value, executedAmount}) {
+        const order = new Order(id, user, type, size, value, executedAmount)
+
+        if(!this.head) {
+            this.head = order
+            this.tail = order
+
+        } else {
+            order.prev = this.tail
+            this.tail.next = order
+
+            this.tail = order
+        }
+        this.length++
+    }
+
+
+
+    insertAtHead({id, user, type, size, value, executedAmount}) {
+        const order = new Order(id, user, type, size, value, executedAmount)
+
+        order.next = this.head
+
+        this.head = order
+        this.length++
+    }
+
+    
+
+    insertAtIndex({id, user, type, size, value, executedAmount}, index) {
+        if(!index) {
+            this.insertAtHead({id, user, type, size, value, executedAmount})
+            
+        } else {
+            const newOrder = new Order(id, user, type, size, value, executedAmount)
+
+            const nOrder = this.getNOrder(index - 1)
+            
+            newOrder.next = nOrder.next
+            nOrder.next = newOrder
+            this.length++
+        }
+
+    }
+
+
+
+    findBy(attr, value) {
+        let order = this.head
+
+        while(order) {
+            if(order[attr] === value) {
+                return order
+            }
+            order = order.next
+        }
+
+        return null
+    }
+}
+
+
+
+
+class Exchange {
+
+    constructor() {
+        this.book = {bid: new Book(), ask: new Book}
+        this.transactions = []
+        this.executedOrders = []
+    }
 
 
 
@@ -13,39 +135,47 @@ class Exchange {
         let executedAmount = 0
         const transactionsOrders = [id]
         const orderMatchWith = {ask: "bid", bid: "ask"}
-        const bookToMatch = orderMatchWith[type]
+        const bookToMatch = this.book[orderMatchWith[type]]
 
-        for(let i = 0; i < this.book[bookToMatch].length && executedAmount < size; i++) {
-            const bookOrder = this.book[bookToMatch][i]
-            if(bookOrder.executed) continue
-            
+        let bookOrder = bookToMatch.head
+        
+        while(bookOrder && executedAmount < size) {
 
-            const price = value/size
-            if(type === "ask" && bookOrder.price < price ||
-               type === "bid" && bookOrder.price > price)
+            if(bookOrder.isExecuted) {
+                bookOrder = bookOrder.next
+                continue
+            }
+
+            const newOrderPrice = value/size
+
+            if(type === "ask" && bookOrder.price < newOrderPrice ||
+               type === "bid" && bookOrder.price > newOrderPrice)
                 break
-            
 
-            const orderRemaining = size - executedAmount
-            const pre_executed = bookOrder.remaining - orderRemaining
+            const newOrderRemaining = size - executedAmount
+            const pre_executed = bookOrder.remainingAmount - newOrderRemaining
             transactionsOrders.push(bookOrder.id)
 
             if(pre_executed > 0) {
-                executedAmount += orderRemaining
-                this.book[bookToMatch][i].remaining -= orderRemaining
+                executedAmount += newOrderRemaining
+                //bookOrder.executedAmount += newOrderRemaining
+                this.book[orderMatchWith[type]].findBy("id", bookOrder.id).executedAmount += newOrderRemaining
 
             } else if(pre_executed === 0) {
-                executedAmount += orderRemaining
-                this.book[bookToMatch][i].remaining = 0
-                this.book[bookToMatch][i].executed = true
+                executedAmount += newOrderRemaining
+                //bookOrder.executedAmount += newOrderRemaining
+                this.book[orderMatchWith[type]].findBy("id", bookOrder.id).executedAmount += newOrderRemaining
+
             
             } else {
-                executedAmount += this.book[bookToMatch][i].remaining
-                this.book[bookToMatch][i].remaining = 0
-                this.book[bookToMatch][i].executed = true
-            }    
-        }
+                executedAmount += bookOrder.remainingAmount
+                //bookOrder.executedAmount += bookOrder.remaining
+                this.book[orderMatchWith[type]].findBy("id", bookOrder.id).executedAmount += bookOrder.remainingAmount
 
+            }
+
+            bookOrder = bookOrder.next
+        }
 
         this.transactions.push({orders: transactionsOrders.sort()})
         return executedAmount
@@ -72,14 +202,16 @@ class Exchange {
 
     canExecute(type, price) {
         if(type === "ask") {
-            const bidBook = this.book.bid.filter(b => !b.executed)
+            //const bidBook = this.book.bid.head
+            const bidBook = this.book.bid.findBy("isExecuted", false)
 
-            return bidBook.length && bidBook[0].price >= price
+            return bidBook && bidBook.price >= price
         }
         else if(type === "bid") {
-            const askBook = this.book.ask.filter(b => !b.executed)
+            //const askBook = this.book.ask.head
+            const askBook = this.book.ask.findBy("isExecuted", false)
 
-            return askBook.length && askBook[0].price <= price
+            return askBook && askBook.price <= price
         }
     }
 
@@ -87,39 +219,50 @@ class Exchange {
 
 
 
-    insertOrderToBook({id, user, type, size, value, price, executed, remaining}) {
-        const order = {id, user, type, size, value, price, executed, remaining}
+    insertOrderToBook({id, user, type, size, value, price, executedAmount}) {
+        const newOrder = {id, user, type, size, value, price, executedAmount}
         const orderBook = this.book[type]
 
         if(!orderBook.length) {
-            this.book[type].push(order)
+            this.book[type].push(newOrder)
         } else {
+            let bookOrder = orderBook.head
+            let bookIndex = 0
 
-            for(let i=0; i<orderBook.length; i++) {
-                const bookOrder = orderBook[i]
-                
-                if(order.price === bookOrder.price) {
-                    if(i+1 === orderBook.length) {
-                        this.book[type].push(order)
+            while(bookOrder) {
+                if(newOrder.price === bookOrder.price) {
+
+                    if(!bookOrder.next) {
+                        orderBook.push(newOrder)
                         return
                     }
 
-                    for(let j=i+1; j<orderBook.length; j++) {
-                        const samePriceOrder = orderBook[j]
-                        if(order.price !== samePriceOrder.price) {
-                            this.book[type].splice(j, 0, order)
+                    let samePriceOrder = bookOrder.next
+                    let samePriceOrderIndex = bookIndex + 1
+
+                    while(samePriceOrder) {
+                        if(samePriceOrder.price !== newOrder.price) {
+                            orderBook.insertAtIndex(newOrder, samePriceOrderIndex)
                             return
                         }
+                        samePriceOrder = samePriceOrderIndex.next
+                        samePriceOrderIndex++
                     }
-                } else if(order.type === "bid" && order.price > bookOrder.price) {
-                    this.book[type].splice(i, 0, order)
+
+                    
+                } else if(newOrder.type === "bid" && newOrder.price > bookOrder.price) {
+                    orderBook.insertAtIndex(newOrder, bookIndex)
                     return
-                } else if(order.type === "ask" && order.price < bookOrder.price) {
-                    this.book[type].splice(i, 0, order)
+                } else if(newOrder.type === "ask" && newOrder.price < bookOrder.price) {
+                    orderBook.insertAtIndex(newOrder, bookIndex)
                     return
                 }
-            }
-            this.book[type].push(order)
+
+                bookOrder = bookOrder.next
+                bookIndex++
+            } 
+
+            this.book[type].push(newOrder)
         }
     }
 
@@ -135,8 +278,7 @@ class Exchange {
             size,
             value,
             price: value/size,
-            executed: false,
-            remaining: size
+            executedAmount: 0
         }
 
 
@@ -146,9 +288,7 @@ class Exchange {
             
         if(this.canExecute(type, order.price)) {
             const executed = this.executeOrder(order.id, type, value, size)
-            order.remaining -= executed
-            
-            if(order.remaining === 0) order.executed = true
+            order.executedAmount = executed
         }
             
         this.insertOrderToBook(order)
@@ -159,16 +299,23 @@ class Exchange {
 
 
     ordersToPrint(type) {
-        return this.book[type].map(e => {
-            return {
-                id: e.id,
-                user: e.user,
-                type,
-                size: e.size,
-                value: e.value,
-                remaining: e.remaining
-            }
-        })
+        const orders = []
+        
+        let order = this.book[type].head
+
+        while(order) {
+            orders.push({
+                id: order.id,
+                user: order.user,
+                type: order.type,
+                size: order.size,
+                value: order.value,
+                remaining: order.remainingAmount
+            })
+            order = order.next
+        }
+
+        return orders
     }
 }
 
